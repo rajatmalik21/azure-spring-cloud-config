1c5d0fc4db1fd655f9182c5028f7666909ef5494
RajatMalik21 <rajat_malik@hotmail.com>
2020-09-16 18:21:02 +0530

    First commit

---
+++ src/main/java/com/example/helloIFI/HelloIfiApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloIFI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloIfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloIfiApplication.class, args);
+	}
+
+}
